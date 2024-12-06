baa1856e9e15e17d74c78ecee702e42bb3dc5ddb
Kai Williams <kailuwilliams@gmail.com>
2024-12-06 13:36:56 -0800

    add arrays

--- lumen.py
+++ lumen.py
@@ -1,5 +1,5 @@
 from textx import metamodel_from_file
-# import psutil
+import psutil
 
 # Load the model from the grammar and input program
 lumen_mm = metamodel_from_file('lumen.tx')
@@ -9,20 +9,16 @@ class Lumen:
     def __init__(self):
         self.varmap = {}
 
-
-    def __str__(self):
-        return f"Robot position is {self.x}, {self.y}."
-
     def interpret(self, model):
-        # model is an instance of Program
         for c in model.statements:
             # Handle different types of statements
             if c.__class__.__name__ == "PrintStatement":
                 self.handle_print(c)
             elif c.__class__.__name__ == "Initialization":
                 self.handle_initialization(c)
-            elif c.__class__.__name__ == "Assignment":
-                self.handle_assignment(c)
+            elif c.__class__.__name__ == "VariableAssignment":
+                print(f"DEBUG: VariableAssignment: varName={c.varName}, value={c.value}")
+                self.handle_variable_assignment(c)
             elif c.__class__.__name__ == "IfStatement":
                 self.handle_if(c)
             elif c.__class__.__name__ == "WhileLoop":
@@ -35,11 +31,12 @@ class Lumen:
                 self.handle_array_assignment(c)
             elif c.__class__.__name__ == "ArrayAccess":
                 self.handle_array_access(c)
+            elif c.__class__.name__ == "ArrayElementAssignment":
+                self.handle_array_assignment(c)
 
     def handle_print(self, command):
         # Print command, supporting variables, arrays, and expressions
         expr = command.expression
-
         if isinstance(expr, str):
             expr = self.evaluate_expression(expr)
             print(expr)  # Direct string
@@ -48,6 +45,12 @@ class Lumen:
         elif isinstance(expr, str) and expr.startswith("["):  # Assuming array
             arr = self.varmap.get(expr, [])
             print(arr)
+        elif expr.__class__.__name__ == "ArrayAccess":
+            array_name = expr.varName
+            index = self.evaluate_expression(expr.index)
+            if array_name not in self.varmap:
+                raise Exception("Array not defined")
+            print(self.varmap[array_name][index])
         else:  # Handle variables
             print(self.varmap.get(expr, "Undefined"))
 
@@ -57,23 +60,28 @@ class Lumen:
 
         if var_name in self.varmap: # Raise Error if already initialized
             raise Exception("Variable is already defined")
-
         self.varmap[var_name] = value
         if isinstance(value, int):
-        	self.varmap[var_name] = value
+            self.varmap[var_name] = value
         elif isinstance(value, str):
             if value.startswith("["):
                 self.varmap[var_name] = list(map(int, value[1:-1].split(",")))
             else:
                 self.varmap[var_name] = eval(value)
+        elif value.__class__.__name__ == "ArrayAccess":
+            if value.varName not in self.varmap:
+                raise Exception("Array not defined")
+            array_name = self.varmap[value.varName]
+            index = self.evaluate_expression.index
+            self.varmap[var_name] = array_name[index]
 
-    def handle_assignment(self, command):
-        # Handle variable assignment
+    def handle_variable_assignment(self, command):
         var_name = command.varName
         value = command.value
-        if isinstance(value, int):  # Direct integer assignment
+
+        if isinstance(value, int):
             self.varmap[var_name] = value
-        elif isinstance(value, str):  # Handle variable or expression assignment
+        elif isinstance(value, str):
             self.varmap[var_name] = self.evaluate_expression(value)
 
     def handle_array_declaration(self, command):
@@ -92,8 +100,31 @@ class Lumen:
         else:
             raise Exception("Array is not defined")
 
-    # def handle_array_access(self, command):
+    def handle_array_element_assignment(self, command):
+        command.expression
+        value = command.value
+        array_name = value.varName
+        index = self.evaluate_expression(value.index)
+        array = self.varmap.get(array_name, [])
 
+        if isinstance(array, list) and 0 <= index < len(array):
+            self.varmap[var_name] = array[index]
+        else:
+            raise ValueError(f"Invalid array access: {array_name}[{index}]")
+
+    def handle_array_access(self, command):
+        array_name = command.varName
+        index = self.evaluate_expression(command.index)
+
+        if array_name not in self.varmap:
+            raise Exception("Array is not defined")
+
+        array_length = len(self.varmap[array_name])
+
+        if index >= array_length or index <= -1:
+            raise Exception("Index out of bounds error")
+
+        return self.varmap[array_name][index]
 
     def handle_if(self, command):
         condition = command.condition
@@ -159,10 +190,6 @@ class Lumen:
 # Interpret the program
 lumen = Lumen()
 lumen.interpret(lumen_model)
-
-# ArrayAccess:
-#   varName=ID '[' index=Expression ']'
-# ;
 
 # FunctionCall:
 #   'CPU.' methodName=ID '(' ')'
